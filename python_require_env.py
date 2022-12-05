89f315b45f7e0e5d6e4b6d21f9387baed4eff569
wenbo13579 <wenbo13579@163.com>
2022-12-05 15:41:32 +0800

    1. 加入github action，自动完成代码校验。

--- python_require_env.py
+++ python_require_env.py
@@ -1,14 +1,19 @@
 import os
+import sys
+
 # ram_report&rom_report need this
 cmd = 'python -m pip install -r scripts/footprint/requirements.txt'
-res = os.popen(cmd).read()
-print(res)
+res = os.system(cmd)
+if res != 0:
+    sys.exit(res)
 # kconfig need this
 cmd = 'python -m pip install -r scripts/kconfig/requirements.txt'
-res = os.popen(cmd).read()
-print(res)
+res = os.system(cmd)
+if res != 0:
+    sys.exit(res)
 
 # docs need this
 cmd = 'python -m pip install -r doc/requirements.txt'
-res = os.popen(cmd).read()
-print(res)
+res = os.system(cmd)
+if res != 0:
+    sys.exit(res)
---
+++ scripts/code_compile_check.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+def compile_code(app, port, chipset):
+    params = (' APP=%s PORT=%s CHIPSET=%s') % (app, port, chipset)
+    # compile code
+    cmd = 'make clean'
+    res = os.system(cmd)
+    if res != 0:
+        return res
+    cmd = 'make all' + params
+    print(cmd)
+    res = os.system(cmd)
+    if res != 0:
+        return res
+
+    return 0
+
+# app_sets = ['beacon', 
+#             'broadcaster', 
+#             'central', 
+#             'central_gatt_write', 
+#             'central_hr', 
+#             'central_ht',
+#             'eddystone',
+#             'ibeacon',
+#             'observer',
+#             'peripheral',
+#             'peripheral_csc',
+#             'peripheral_dis',
+#             'peripheral_esp',
+#             'peripheral_gatt_write',
+#             'peripheral_hids',
+#             'peripheral_hr',
+#             'peripheral_ht',
+#             'peripheral_throughput', ]
+app_sets = ['beacon']
+
+port_sets = ['windows_libusb_win32', 
+             'windows_serial', ]
+
+# chipset_sets = ['ats2851', 
+#                 'common', 
+#                 'csr8510', 
+#                 'csr8910', 
+#                 'pts_dongle',]
+chipset_sets = ['ats2851',]
+
+if __name__ == '__main__':
+    for app in app_sets:
+        for port in port_sets:
+            for chipset in chipset_sets:
+                res = compile_code(app, port, chipset)
+                if(res != 0):
+                    sys.exit(res)
+    
+    sys.exit(0)
